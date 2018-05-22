a20717aa04b037c5ee8342c7b4f480a341c477b8
John Tuttle <jtuttle@conjur.net>
2018-05-22 15:36:47 -0400

    Conj 5038  sidecar auth container can be run as an init container (#2)
    * split out init container manifest, reorganize a bit
    
    * working init container example

--- 6_deploy_test_app.sh
+++ 6_deploy_test_app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+set -eo pipefail
 
 . utils.sh
 
@@ -19,14 +19,27 @@ if ! [ "${DOCKER_EMAIL}" = "" ]; then
     --docker-email=$DOCKER_EMAIL
 fi
 
-kubectl delete --ignore-not-found deployment test-app
-kubectl delete --ignore-not-found service test-app
+kubectl delete --ignore-not-found deployment test-app-api-sidecar
+kubectl delete --ignore-not-found service test-app-api-sidecar
+kubectl delete --ignore-not-found serviceaccount test-app-api-sidecar
+
+kubectl delete --ignore-not-found deployment test-app-api-init
+kubectl delete --ignore-not-found service test-app-api-init
+kubectl delete --ignore-not-found serviceaccount test-app-api-init
 
 sleep 5
 
 test_app_docker_image=$DOCKER_REGISTRY_PATH/test-app:$CONJUR_NAMESPACE_NAME
 
-sed -e "s#{{ TEST_APP_DOCKER_IMAGE }}#$test_app_docker_image#g" ./test_app/test_app.yaml |
+sed -e "s#{{ TEST_APP_DOCKER_IMAGE }}#$test_app_docker_image#g" ./manifests/test-app-api-sidecar.yaml |
+  sed -e "s#{{ CONJUR_ACCOUNT }}#$CONJUR_ACCOUNT#g" |
+  sed -e "s#{{ CONJUR_NAMESPACE_NAME }}#$CONJUR_NAMESPACE_NAME#g" |
+  sed -e "s#{{ TEST_APP_NAMESPACE_NAME }}#$TEST_APP_NAMESPACE_NAME#g" |
+  sed -e "s#{{ SERVICE_ID }}#$AUTHENTICATOR_SERVICE_ID#g" |
+  sed -e "s#{{ CONFIG_MAP_NAME }}#$TEST_APP_NAMESPACE_NAME#g" |
+  kubectl create -f -
+
+sed -e "s#{{ TEST_APP_DOCKER_IMAGE }}#$test_app_docker_image#g" ./manifests/test-app-api-init.yaml |
   sed -e "s#{{ CONJUR_ACCOUNT }}#$CONJUR_ACCOUNT#g" |
   sed -e "s#{{ CONJUR_NAMESPACE_NAME }}#$CONJUR_NAMESPACE_NAME#g" |
   sed -e "s#{{ TEST_APP_NAMESPACE_NAME }}#$TEST_APP_NAMESPACE_NAME#g" |
