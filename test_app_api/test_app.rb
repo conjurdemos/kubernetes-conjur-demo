620b530576d77f4f7ff9692166f76874e5aba738
John Tuttle <jtuttle@conjur.net>
2018-06-19 10:23:00 -0400

    use 403 status

--- test_app_api/test_app.rb
+++ test_app_api/test_app.rb
@@ -43,7 +43,7 @@ get '/' do
   rescue RestClient::Forbidden => e
     $stderr.puts $!
     $stderr.puts $!.backtrace.join("\n")
-    halt 500, "Error: Host #{access_token['data']} does not have access to variable #{variable}."
+    halt 403, "Error: Host #{access_token['data']} does not have access to variable #{variable}."
   end
 
   "test-app-db password: #{value}"
