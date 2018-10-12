6af52db2d9e8aab3bcb649ec1ac9b251eea88f7f
Geri Jennings <geri.jennings@cyberark.com>
2018-10-12 16:51:12 -0400

    Push multiple app images for OC

--- 5_build_and_push_containers.sh
+++ 5_build_and_push_containers.sh
@@ -9,15 +9,15 @@ fi
 
 announce "Building and pushing test app images."
 
+readonly APPS=(
+  "init"
+  "sidecar"
+)
+
 # Kubernetes and OpenShift currently run different apps in the demo
 if [[ "$PLATFORM" = "kubernetes" ]]; then
 
   pushd test_app_summon
-    readonly APPS=(
-      "init"
-      "sidecar"
-    )
-
     for app_type in "${APPS[@]}"; do
       # prep secrets.yml
       sed -e "s#{{ TEST_APP_NAME }}#test-summon-$app_type-app#g" ./secrets.template.yml > secrets.yml
@@ -48,11 +48,14 @@ else
 
   pushd webapp
     ./build.sh
-    test_app_image=$(platform_image test-app)
-    docker tag test-app:$CONJUR_NAMESPACE_NAME $test_app_image
-    if [[ is_minienv != true ]]; then
-      docker push $test_app_image
-    fi
+
+    for app_type in "${APPS[@]}"; do
+      test_app_image=$(platform_image "test-$app_type-app")
+      docker tag test-app:$CONJUR_NAMESPACE_NAME $test_app_image
+      if [[ is_minienv != true ]]; then
+        docker push $test_app_image
+      fi
+    done
   popd
 
 fi
