1a67b2674d016eed996c66169a1f8b2ba2b1448d
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2019-03-19 01:33:53 +0800

    redefine ready

--- 7_verify_authentication.sh
+++ 7_verify_authentication.sh
@@ -80,9 +80,9 @@ function split_url() {
 }
 
 echo "Waiting for urls to be ready"
-while [[ ! $(nc -z $(split_url $init_url)) ]] ||
-      [[ ! $(nc -z $(split_url $sidecar_url)) ]] ||
-      [[ ! $(nc -z $(split_url $secretless_url)) ]]; do
+while ! $(nc -z $(split_url $init_url)) ||
+      ! $(nc -z $(split_url $sidecar_url)) ||
+      ! $(nc -z $(split_url $secretless_url)); do
   printf "."
   sleep 3
 done
