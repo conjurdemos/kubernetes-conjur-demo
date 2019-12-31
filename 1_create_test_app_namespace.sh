c5170c0d7c75166d4abca6539de036dd53ced16d
orenbm <oren.benmeir@cyberark.com>
2019-12-31 16:23:08 +0200

    Remove redundant RoleBinding from test env
    We were loading a RoleBinding that is related to the Conjur
    namespace and not the test namespace. This load was moved
    to `kubernetes-conjur-deply` and is not needed here anymore.

--- 1_create_test_app_namespace.sh
+++ 1_create_test_app_namespace.sh
@@ -28,10 +28,6 @@ fi
 
 $cli delete --ignore-not-found rolebinding test-app-conjur-authenticator-role-binding-$CONJUR_NAMESPACE_NAME
 
-sed "s#{{ TEST_APP_NAMESPACE_NAME }}#$TEST_APP_NAMESPACE_NAME#g" ./$PLATFORM/test-app-conjur-authenticator-role-binding.yml |
-  sed "s#{{ CONJUR_NAMESPACE_NAME }}#$CONJUR_NAMESPACE_NAME#g" |
-  $cli create -f -
-
 if [[ $PLATFORM == openshift ]]; then
   # add permissions for Conjur admin user
   oc adm policy add-role-to-user system:registry $OSHIFT_CONJUR_ADMIN_USERNAME
